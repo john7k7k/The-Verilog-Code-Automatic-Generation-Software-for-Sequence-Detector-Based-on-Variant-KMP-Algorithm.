57029188b0df9518594658ee92d55577def400d0
Your Name <you@example.com>
2024-03-12 21:13:19 +0800

    debug contunue

--- dec/testbench.v
+++ dec/testbench.v
@@ -13,37 +13,16 @@ module tb_SequenceDetector;
         .detected(detected)
     );
     always #((CLK_PERIOD/2)) clk = ~clk;
+    always #10 data_in=$random%3;
     initial begin
         $dumpfile("dec/tb.vcd");
         $dumpvars(0, dut);
         clk = 1;
         reset = 1;
         data_in = 0;
-        #10;
+        #10
         reset = 0;
-        #10;
-        data_in = 1;
-        #10;
-        data_in = 0;
-        #10;
-        data_in = 1;
-        #10;
-        data_in = 0;
-        #10;
-        data_in = 1;
-        #10;
-        data_in = 0;
-        #10;
-        data_in = 0;
-        #10;
-        data_in = 0;
-        #10;
-        data_in = 0;
-        #10;
-        data_in = 1;
-        #10;
-        data_in = 0;
-        #10;
+        #1000
         $finish;
     end
 endmodule
